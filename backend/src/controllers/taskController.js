bcb91d3382fecda0d1716f668fa23b26254dfa20
Imran <imranrod31@gmail.com>
2026-05-14 15:41:02 +0100

    fix: update populate to use fullName instead of nom

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,17 +1,15 @@
 const Task = require('../models/Task');
 
-// GET - jib tasks dyal projet
 const getTasks = async (req, res) => {
   try {
     const tasks = await Task.find({ projet: req.params.id })
-      .populate('assignedTo', 'nom email');
+      .populate('assignedTo', 'fullName email');
     res.json(tasks);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
-// POST - khla9 task jdida
 const createTask = async (req, res) => {
   try {
     const task = new Task({
@@ -25,7 +23,6 @@ const createTask = async (req, res) => {
   }
 };
 
-// PUT - 3del task
 const updateTask = async (req, res) => {
   try {
     const task = await Task.findByIdAndUpdate(
@@ -39,7 +36,6 @@ const updateTask = async (req, res) => {
   }
 };
 
-// DELETE - 7ed task
 const deleteTask = async (req, res) => {
   try {
     await Task.findByIdAndDelete(req.params.id);
@@ -49,7 +45,6 @@ const deleteTask = async (req, res) => {
   }
 };
 
-// PATCH - 3del statut fqt
 const updateTaskStatus = async (req, res) => {
   try {
     const task = await Task.findByIdAndUpdate(
@@ -63,7 +58,6 @@ const updateTaskStatus = async (req, res) => {
   }
 };
 
-// GET - jib tasks dyal user connecté filtré b projet w membre
 const getMyTasks = async (req, res) => {
   try {
     const filter = { assignedTo: req.user.id };
@@ -77,7 +71,7 @@ const getMyTasks = async (req, res) => {
     }
 
     const tasks = await Task.find(filter)
-      .populate('assignedTo', 'nom email')
+      .populate('assignedTo', 'fullName email')
       .sort({ priorite: -1, dateLimite: 1 });
       
     res.json(tasks);
@@ -86,14 +80,13 @@ const getMyTasks = async (req, res) => {
   }
 };
 
-// PUT - assign task l membre
 const assignTask = async (req, res) => {
   try {
     const task = await Task.findByIdAndUpdate(
       req.params.id,
       { assignedTo: req.body.userId },
       { new: true }
-    ).populate('assignedTo', 'nom email');
+    ).populate('assignedTo', 'fullName email');
     res.json(task);
   } catch (err) {
     res.status(400).json({ message: err.message });
